7de55ee725eab8a180a1d1e2348d475d8051cd27
Andrey T <andrey.troyan77@gmail.com>
2025-09-26 18:55:49 +0300

    fix: логика if должна быть в сервисном слое, добавить интерфейс CarService и класс CarServiceImpl. сделать CarController отдельно.
        - Добавил интерфейс CarService и класс CarServiceImpl
        - Логику метода showCars() прописал в классе CarServiceImpl
        - Сделал отдельно контроллер для CarController
        - Поправил предупреждения среды разработки (в жёлтом треугольнике восклищательные знаки)

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,46 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
-public class HelloController {
+public class CarController {
 
     private final CarServiceImpl carServiceImpl;
 
     @Autowired
-    public HelloController(CarServiceImpl carServiceImpl) {
+    public CarController(CarServiceImpl carServiceImpl) {
         this.carServiceImpl = carServiceImpl;
     }
-
-    @GetMapping(value = "/")
-    public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("I'm Spring MVC application");
-        messages.add("5.2.0 version by sep'19 ");
-        model.addAttribute("messages", messages);
-        return "index";
-    }
-
-//	@GetMapping(value = "/cars")
-//	public String printCars(ModelMap model) {
-//		model.addAttribute("cars", carDAO.carsList());
-//		return "cars";
-//	}
-
-
-//	@GetMapping(value = "/cars/{id}")
-//	public String show(@PathVariable("id") int id, ModelMap model) {
-//		model.addAttribute("carsCount", carDAO.showCars(id));
-//		return "show";
-//	}
 
     @GetMapping(value = "/cars")
     public String show(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
         model.addAttribute("cars", carServiceImpl.showCars(count));
-        return "cars";
+        return "CarService/cars";
     }
 
 }
--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,24 +1,14 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
 
-    private final CarServiceImpl carServiceImpl;
-
-    @Autowired
-    public HelloController(CarServiceImpl carServiceImpl) {
-        this.carServiceImpl = carServiceImpl;
-    }
 
     @GetMapping(value = "/")
     public String printWelcome(ModelMap model) {
@@ -27,14 +17,6 @@ public class HelloController {
         messages.add("I'm Spring MVC application");
         messages.add("5.2.0 version by sep'19 ");
         model.addAttribute("messages", messages);
-        return "index";
+        return "Hello/index";
     }
-
-
-    @GetMapping(value = "/cars")
-    public String show(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
-        model.addAttribute("cars", carServiceImpl.showCars(count));
-        return "cars";
-    }
-
 }
