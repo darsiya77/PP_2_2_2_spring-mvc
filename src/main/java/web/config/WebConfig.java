ac088dfdac149eb241f264875347b443cb7547b4
Andrey T <andrey.troyan77@gmail.com>
2025-09-26 14:22:05 +0300

    - All set!

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -25,25 +25,33 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
+
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/pages/");
         templateResolver.setSuffix(".html");
+
         return templateResolver;
     }
 
     @Bean
     public SpringTemplateEngine templateEngine() {
+
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
+
         return templateEngine;
     }
 
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
+
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
     }
--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,9 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.dao.CarDAO;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +14,44 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-	@GetMapping(value = "/")
-	public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "index";
-	}
-	
+    private final CarDAO carDAO;
+
+    @Autowired
+    public HelloController(CarDAO carDAO) {
+        this.carDAO = carDAO;
+    }
+
+    @GetMapping(value = "/")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+
+//	@GetMapping(value = "/cars")
+//	public String printCars(ModelMap model) {
+//		model.addAttribute("cars", carDAO.carsList());
+//		return "cars";
+//	}
+
+
+//	@GetMapping(value = "/cars/{id}")
+//	public String show(@PathVariable("id") int id, ModelMap model) {
+//		model.addAttribute("carsCount", carDAO.showCars(id));
+//		return "show";
+//	}
+
+    @GetMapping(value = "/cars")
+    public String show(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
+        if (count != null) {
+            model.addAttribute("cars", carDAO.showCars(count));
+        } else {
+            model.addAttribute("cars", carDAO.carsList());
+        }
+        return "cars";
+    }
+
 }
