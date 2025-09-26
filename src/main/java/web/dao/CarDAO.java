c18f30105c991f8711db438866c710372d6a9e31
Andrey T <andrey.troyan77@gmail.com>
2025-09-26 14:22:05 +0300

    - All set

--- src/main/java/web/dao/CarDAO.java
+++ src/main/java/web/dao/CarDAO.java
@@ -1,4 +1,32 @@
 package web.dao;
 
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class CarDAO {
+    private static int CAR_COUNT;
+    private List<Car> cars;
+
+    {
+        this.cars = new ArrayList<Car>();
+        this.cars.add(new Car(++CAR_COUNT, "BMW", "Black"));
+        this.cars.add(new Car(++CAR_COUNT, "Audi", "Yellow"));
+        this.cars.add(new Car(++CAR_COUNT, "Ferrari", "Red"));
+        this.cars.add(new Car(++CAR_COUNT, "Mercedes", "Blue"));
+        this.cars.add(new Car(++CAR_COUNT, "Toyota", "Silver"));
+    }
+
+    public List<Car> carsList() {
+        return this.cars;
+    }
+
+    public List<Car> showCars(int count) {
+
+        return cars.stream().limit(Math.min(count, cars.size())).collect(Collectors.toList());
+    }
 }
